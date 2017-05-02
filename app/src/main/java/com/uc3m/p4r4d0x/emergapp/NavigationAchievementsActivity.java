c58f275bd813dde53992912d61b2a4fbc3b5573d
aloarte <aloarter@gmail.com>
2017-05-02 17:24:22 +0200

    Cambiado el toolbar a uno genérico manejado por una activity común

---
+++ app/src/main/java/com/uc3m/p4r4d0x/emergapp/NavigationAchievementsActivity.java
@@ -0,0 +1,15 @@
+package com.uc3m.p4r4d0x.emergapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.p4r4d0x.eReport.R;
+
+public class NavigationAchievementsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation_achievements);
+    }
+}
---
+++ app/src/main/java/com/uc3m/p4r4d0x/emergapp/NavigationActivityInner.java
@@ -0,0 +1,31 @@
+package com.uc3m.p4r4d0x.emergapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.p4r4d0x.eReport.R;
+
+public class NavigationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
